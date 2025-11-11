843a8e2ff8f702678b63906c288641cac8bb54a6
Askia VAN RYCKEGHEM <skiaa@hotmail.com>
2025-11-11 09:52:35 +0100

    Update seed_data.sql

--- postgresql/schema/seed_data.sql
+++ postgresql/schema/seed_data.sql
@@ -6,10 +6,10 @@ INSERT INTO clients (nom, email) VALUES
 
 -- Insertion de produits
 INSERT INTO produits (nom, categorie, prix, stock) VALUES
-('Ordinateur portable', 'Informatique', 899.99, 10),
-('Smartphone', 'Téléphonie', 599.99, 25),
-('Casque audio', 'Accessoires', 79.99, 50),
-('Clavier mécanique', 'Informatique', 129.99, 15);
+('Macbook pro 2025', 'Informatique', 1899.99, 10),
+('Samsung Galaxy S25', 'Téléphonie', 999.99, 25),
+('Jabra Evolve 75', 'Accessoires', 79.99, 50),
+('Roccat Vulkan 120', 'Informatique', 129.99, 15);
 
 -- Insertion de commandes
 INSERT INTO commandes (client_id, statut) VALUES
@@ -19,7 +19,7 @@ INSERT INTO commandes (client_id, statut) VALUES
 
 -- Insertion de lignes de commande
 INSERT INTO lignes_commandes (commande_id, produit_id, quantite, prix_unitaire) VALUES
-(1, 1, 1, 899.99),   -- Alice a acheté un ordinateur portable
+(1, 1, 1, 1899.99),   -- Alice a acheté un ordinateur portable
 (1, 3, 2, 79.99),    -- Alice a aussi pris 2 casques audio
-(2, 2, 1, 599.99),   -- Bob a commandé un smartphone
+(2, 2, 1, 999.99),   -- Bob a commandé un smartphone
 (3, 4, 1, 129.99);   -- Alice a commandé un clavier mécanique
